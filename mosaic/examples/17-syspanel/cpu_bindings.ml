6876c2e1eb2ea794fb2bef22df9bc7b614d12b8d
arsalaan <69048139+Arsalaan-Alam@users.noreply.github.com>
2025-12-31 18:27:02 +0530

    remove duplicate

--- mosaic/examples/17-syspanel/cpu_bindings.ml
+++
@@ -1,6 +0,0 @@
-(** OCaml bindings for macOS CPU statistics using host_statistics *)
-
-external get_cpu_load : unit -> int64 * int64 * int64 * int64 = "ocaml_get_cpu_load"
-
-let get_cpu_load () = get_cpu_load ()
-
